a37e29b6db35b1d87bc5098928f54970c2ec90e4
Kyle Chmielewski <kyle.chmielewski@us.abnamroclearing.com>
2020-07-07 10:59:57 -0500

    --Change column sizes on components --Add ingredients.model.ts --Add some input commands to shopping edit

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
-  constructor(name: string, desc: string, path: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = path;
-  }
+  constructor(public name: string, public description: string, public imagePath: string) {  }
 }
