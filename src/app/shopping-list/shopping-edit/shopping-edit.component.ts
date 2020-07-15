aeb7c9c3c3af99fb923ea26f73ce53f1e753b742
Kyle Chmielewski <kyle.chmielewski@us.abnamroclearing.com>
2020-07-15 12:05:04 -0500

    --Complete Shopping-List.html form, button clicks

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,66 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { TitleCasePipe } from '@angular/common';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
-  @ViewChild('quantityInput', { static: false }) quantityInput: ElementRef;
-
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('formElement') formElement: NgForm;
+  editingSubscription: Subscription;
+  editMode = false;
+  editingItemIndex: number;
+  editingItem: Ingredient;
+  
 
   constructor(private titleCasePipe: TitleCasePipe,
     private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.editingSubscription = this.shoppingListService.startedEditing
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editingItemIndex = index;
+        this.editingItem = this.shoppingListService.getIngredient(index);
+        this.formElement.setValue({
+          name: this.editingItem.name,
+          quantity: this.editingItem.quantity
+        })
+      });
   }
 
-  onAddItem() {
-    const newIngredientName = this.titleCasePipe.transform(this.nameInput.nativeElement.value);
-    const newIngredient = new Ingredient(newIngredientName, this.quantityInput.nativeElement.value);
-    this.shoppingListService.addIngredient(newIngredient)
+  onSubmit(form: NgForm) {
+    //name & quantity come from name="" field in .html
+    const value = form.value;
+    const newIngredientName = this.titleCasePipe.transform(value.name);
+    const newIngredient = new Ingredient(newIngredientName, value.quantity);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editingItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.formElement.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editingItemIndex);
+    this.onClear();
+  }
+
+
+  ngOnDestroy() {
+    this.editingSubscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -10,36 +10,45 @@ export class ShoppingListService {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)];
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   constructor() { }
 
+  ////////////GET
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   getIngredients() {
-    //will return a copy of the array so we're not returning original
+    //will return a (not deep)copy of the array so we're not returning original
     return this.ingredients.slice();
   }
 
+  /////////////ADD
+
   addIngredient(ingredient: Ingredient) {
-    this.normalizeAndAddIngredient(ingredient);
+    this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
-    for (let ingredient of ingredients) {
-      this.normalizeAndAddIngredient(ingredient);
-    }
     //...splits array of objects into a list of individual objects
-    //this.ingredients.push(...ingredients);
+    this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
-  normalizeAndAddIngredient(ingredient: Ingredient) {
-    if (this.ingredients.some(i => i.name === ingredient.name)) {
-      const idx = this.ingredients.findIndex(i => i.name === ingredient.name);
-      if (idx >= 0) {
-        this.ingredients[idx].quantity = +this.ingredients[idx].quantity + +ingredient.quantity;
-      }
-    } else {
-      this.ingredients.push(ingredient);
-    }
+  /////////////UDPATE
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  /////////////DELETE
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
