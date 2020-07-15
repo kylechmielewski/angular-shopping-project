e5fe86542b8608d91f4a808923764d0b05cb0f24
Kyle Chmielewski <kyle.chmielewski@us.abnamroclearing.com>
2020-07-15 16:00:34 -0500

    --Creating New Recipe input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TitleCasePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
@@ -34,7 +34,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutesModule
+    AppRoutesModule,
+    ReactiveFormsModule
   ],
   providers: [TitleCasePipe,
     ShoppingListService],
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -28,13 +28,17 @@ export class ShoppingListService {
   /////////////ADD
 
   addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.normalizeAndAddIngredient(ingredient);
+    //this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
+    for (let ingredient of ingredients) {
+      this.normalizeAndAddIngredient(ingredient);
+    }
     //...splits array of objects into a list of individual objects
-    this.ingredients.push(...ingredients);
+    //this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
@@ -50,5 +54,18 @@ export class ShoppingListService {
   deleteIngredient(index: number) {
     this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  ////////////////////
+
+  normalizeAndAddIngredient(ingredient: Ingredient) {
+    if (this.ingredients.some(i => i.name === ingredient.name)) {
+      const idx = this.ingredients.findIndex(i => i.name === ingredient.name);
+      if (idx >= 0) {
+        this.ingredients[idx].quantity = +this.ingredients[idx].quantity + +ingredient.quantity;
+      }
+    } else {
+      this.ingredients.push(ingredient);
+    }
   }
 }
