e4473ca9e2247157ccc0b14a37900f1f4e746bf5
Kyle Chmielewski <kyle.chmielewski@us.abnamroclearing.com>
2020-07-23 13:39:33 -0500

    Add lazy loading to modules

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { AuthComponent } from './auth/auth.component';
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },  //only redirect if the full path is empty
-  { path: 'auth', component: AuthComponent }
+  { 
+    path: 'recipes', 
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) //For lazy loading
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) //For lazy loading
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) //For lazy loading
+  }
 ];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes)
+    //Once we are on home page, all other modules will be loaded while user browses page
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+    //RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
