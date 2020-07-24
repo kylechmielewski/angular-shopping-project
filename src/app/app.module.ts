2d455a3aa4ea4fac66e1b7f596b9c34aaf162df2
Kyle Chmielewski <kyle.chmielewski@us.abnamroclearing.com>
2020-07-24 16:57:56 -0500

    Add @angular/platform-browser/animations to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,6 +15,7 @@ import { CoreModule } from './core.module';
     HeaderComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutesModule,
